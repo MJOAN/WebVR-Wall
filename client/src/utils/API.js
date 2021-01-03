a92b94e6eb5156136bf7435f8e0a2b74e33f8a9e
Mariam Joan <royalton@gmail.com>
2021-01-03 14:17:36 -0800

    heroku login

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,10 +3,10 @@ import axios from "axios";
 export default {
   // gets all profiles
   getProfiles: function() {
-    return axios.get("http://motivational-vr-wall.herokuapp.com/api");
+    return axios.get("https://motivational-vr-wall.herokuapp.com/api");
   },
   // gets profile with given id
   getProfile: function(id) {
-return axios.get("http://motivational-vr-wall.herokuapp.com/api/" + id);
+return axios.get("https://motivational-vr-wall.herokuapp.com/api/" + id);
   }
 };
